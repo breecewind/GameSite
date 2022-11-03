41852c0d6ef613a593a657b1fe83c66e1eb99b57
Reece Windschitl <91442166+FleeceReece@users.noreply.github.com>
2022-11-03 09:42:54 -0400

    Add files via upload

---
+++ games/CellMachine/mods/mystic/export.js
@@ -0,0 +1,20 @@
+import Push from "./cells/push.js";
+import Slide from "./cells/slide.js";
+import Mover from "./cells/mover.js";
+import Rotator from "./cells/rotator.js";
+import Rotator_CCW from "./cells/rotator_ccw.js";
+import Generator from "./cells/generator.js";
+import Trash from "./cells/trash.js";
+import Immobile from "./cells/immobile.js";
+import Enemy from "./cells/enemy.js";
+export default [
+    Push,
+    Slide,
+    Mover,
+    Rotator,
+    Rotator_CCW,
+    Generator,
+    Trash,
+    Immobile,
+    Enemy
+];
