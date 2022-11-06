91382260be8664183521a7fdfb897bf104275c24
Reece Windschitl <91442166+FleeceReece@users.noreply.github.com>
2022-11-06 12:26:44 -0500

    Delete games/CellMachine directory

--- games/CellMachine/mods/jell/export.js
+++
@@ -1,6 +0,0 @@
-import Arrow from "./cells/arrow.js";
-import Orientator from "./cells/orientator.js";
-export default [
-    Arrow,
-    Orientator
-];
--- games/CellMachine/mods/mystic/cells/push.js
+++
@@ -1,8 +0,0 @@
-import Cell from "../../../cell.js";
-export default class Push extends Cell {
-    constructor(x = 0, y = 0, rot = 0, sys) {
-        super(x, y, rot, sys);
-    }
-    static type = "push";
-    static texture = "./mods/mystic/tex/push.png";
-}
